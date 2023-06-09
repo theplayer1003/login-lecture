4b91caa8cb70e4db3f9f74ce295232c4c14f9603
theplayer1003 <theplayer1003@gmail.com>
2023-06-09 14:51:46 +0900

    dom 15/44

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,19 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+
+
+//DOM : Document Object Model, interface, scan html data
+
+const id = document.querySelector("#id"),
+    psword = document.querySelector("#psword"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psword: psword.value,
+    };
+    
+}
