c5d9d31555ac16577f90ea325669730f37f4d5df
theplayer1003 <theplayer1003@gmail.com>
2023-06-18 19:30:40 +0900

    23/44 SIGN-UP-PAGE

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl");
 //route경로로 요청. 슬래시는 루트를 의미함.
 router.get("/",ctrl.output.home);
 router.get("/login",ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login",ctrl.process.login);
 
 module.exports = router;
