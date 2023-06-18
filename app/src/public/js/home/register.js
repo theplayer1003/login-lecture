77e5221044704ab26c509c176ad60d8c3dfb9525
theplayer1003 <theplayer1003@gmail.com>
2023-06-18 19:58:29 +0900

    24/44 registerpage

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -5,19 +5,23 @@
 //DOM : Document Object Model, interface, scan html data
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button");
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login() {
+function register() {
     const req = {
         id: id.value,
+        name: name.value,
         psword: psword.value,
+        confirmPsword: confirmPsword.value,
     };
 
     
-    fetch("/login", {
+    fetch("/register", {
         method: "POST",
         headers:{
             "Content-Type": "application/json",
@@ -27,12 +31,12 @@ function login() {
     .then((res) => res.json())
     .then((res) => {
         if(res.success){
-            location.href = "/";
+            location.href = "/login";
         }else {
             alert(res.msg);
         }
     })
     .catch((err) => {
-     console.error(new Error("로그인 에러"));
+     console.error(new Error("회원가입 에러"));
     });
 }
